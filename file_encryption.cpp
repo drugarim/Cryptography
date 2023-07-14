2570f8e709d2992e035fed30da529890fc563d7a
Manzi Dave Rugari <138680051+drugarim@users.noreply.github.com>
2023-07-14 13:44:48 +0200

    Update file_encryption.cpp
    version 3

--- file_encryption.cpp
+++ file_encryption.cpp
@@ -11,6 +11,9 @@ Botan::System_RNG rng;
 // AES-256 Initialization Vector size in bytes (128/8=16)
 size_t AES_IV_LENGTH = 16;
 
+// AES-256 Initialization Vector size in bytes (128/8=16)
+size_t BLOCK_SIZE = 4096;
+
 // AES-256 key size in bytes (256/8=32)
 size_t AES_KEY_LENGTH = 32;
 
@@ -25,20 +28,17 @@ void encrypt(const std::string &plaintextFilename, const std::string &ciphertext
     enc->set_key(key);
 
     // Read data from input file and encrypt it
-    Botan::secure_vector<uint8_t> buffer(4096); // Buffer size for reading data
+    Botan::secure_vector<uint8_t> buffer(BLOCK_SIZE); // Buffer size for reading data
 
     // Header for GCM
-    // outputFile.write(reinterpret_cast<const char*>(iv.data()), iv.size());
+    outputFile.write(reinterpret_cast<const char *>(iv.data()), AES_IV_LENGTH);
 
     while (!inputFile.eof())
     {
-        // std::basic_istream<reinterpret_cast<char > data = inputFile.read(reinterpret_cast<char *>(buffer.data()), buffer.size());
-        std::istream &data = inputFile.read(reinterpret_cast<char *>(buffer.data()), buffer.size());
-        buffer.resize(inputFile.gcount());
-        if (!data.eof()){
-            break;
-        }
-        // size_t bytesRead = inputFile.gcount();
+        inputFile.read(reinterpret_cast<char *>(buffer.data()), buffer.size());
+        size_t bytesRead = inputFile.gcount();
+
+        buffer.resize(bytesRead);
         enc->start(iv);
         enc->finish(buffer);
 
@@ -50,18 +50,18 @@ void encrypt(const std::string &plaintextFilename, const std::string &ciphertext
     outputFile.close();
 }
 
-void startEncryption(std::string plaintextFilename, std::string plaintextContent, std::string ciphertextFilename, const std::vector<uint8_t> &key, const std::vector<uint8_t> &iv)
+void startEncryption(std::string plaintextFilename, std::string ciphertextFilename, const std::vector<uint8_t> &key, const std::vector<uint8_t> &iv)
 {
     // // Write plain text to input file
     // std::ofstream inputFile(plaintextFilename, std::ios::binary);
     // inputFile.write(plaintextContent.c_str(), plaintextContent.size());
     // inputFile.close();
-    
+
     // Encryption of file
     encrypt(plaintextFilename, ciphertextFilename, key, iv);
-
-    //Displays the contents of the encrypted file on terminal
     std::cout << "File encrypted." << std::endl;
+
+    // Displays the contents of the encrypted file on terminal
     std::ifstream encryptedFile(ciphertextFilename, std::ios::binary);
     std::string encryptedData((std::istreambuf_iterator<char>(encryptedFile)), std::istreambuf_iterator<char>());
     encryptedFile.close();
@@ -74,22 +74,25 @@ void decrypt(const std::string &ciphertextFilename, const std::string &decryptio
     std::ifstream ciphertextStream(ciphertextFilename, std::ios::binary);
     std::ofstream decryptionResultStream(decryptionResultFile, std::ios::binary);
 
+    // Read the header for GCM
+    std::vector<uint8_t> header(AES_IV_LENGTH);
+    ciphertextStream.read(reinterpret_cast<char *>(header.data()), AES_IV_LENGTH);
+
     // Create the object that performs the decryption (AES-256,GCM mode)
     std::unique_ptr<Botan::Cipher_Mode> decrypt = Botan::Cipher_Mode::create("AES-256/GCM", Botan::Cipher_Dir::DECRYPTION);
     decrypt->set_key(key);
 
     // Read data from input file and decrypt it
-    Botan::secure_vector<uint8_t> ciphertextBuffer(4096); // Buffer size for reading data
+    Botan::secure_vector<uint8_t> ciphertextBuffer(BLOCK_SIZE + 16); // Buffer size for reading data
 
     while (!ciphertextStream.eof())
     {
-        std::istream &data = ciphertextStream.read(reinterpret_cast<char *>(ciphertextBuffer.data()), ciphertextBuffer.size());
-        ciphertextBuffer.resize(ciphertextStream.gcount());
-        // size_t bytesRead = ;
-        if (!data.eof()){
-            break;
-        }
-        decrypt->start(iv);
+        ciphertextStream.read(reinterpret_cast<char *>(ciphertextBuffer.data()), ciphertextBuffer.size());
+        size_t bytesRead = ciphertextStream.gcount();
+
+        ciphertextBuffer.resize(bytesRead);
+        // Set the IV from the header
+        decrypt->start(header);
         decrypt->finish(ciphertextBuffer);
 
         // Write decrypted data to output file
@@ -114,12 +117,8 @@ void startDecryption(std::string ciphertextFilename, std::string decryptionResul
     std::cout << "Decrypted data: " << decryptedData << std::endl;
 }
 
-
-
 int main()
 {
-    // std::string newFileName = "newFile.txt";
-
     // Create Initialization Vector by randomizing an array of 256 bits
     std::vector<uint8_t> key(AES_KEY_LENGTH);
     rng.randomize(key.data(), key.size());
@@ -129,13 +128,16 @@ int main()
     rng.randomize(iv.data(), iv.size());
 
     // Encrytion
-    std::string plaintextFilename = "plaintext.txt";
+    // std::string plaintextFilename = "plaintext.txt";
+    // std::string plaintextContent = "Cryptography is really hard!";
     std::string ciphertextFilename = "ciphertext.txt";
-    std::string plaintextContent = "Cryptography is really hard!";
+
     // ask the user what file they want to encrypt
-    // std::cout << "Which file would you like to encrypt: " << std::endl;
+    std::string plaintextFilename = "dave.txt";
+    std::cout << "Which file would you like to encrypt: " << std::endl;
     // std::cin >> plaintextFilename;
-    startEncryption(plaintextFilename, plaintextContent, ciphertextFilename, key, iv);
+
+    startEncryption(plaintextFilename, ciphertextFilename, key, iv);
 
     // Decryption
     std::string decryptionResultFile = "result.txt";
